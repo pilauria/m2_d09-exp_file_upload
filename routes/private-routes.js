83263884c3eaca0ea6d4158179f7bce0cb4a46ad
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-11-22 14:03:50 +0100

    starter code

--- routes/private-routes.js
+++ routes/private-routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const Room = require('./../models/Room.model');
-const fileUploader = require('./../config/cloudinary')
 
 router.get('/profile', (req, res) => {
 	res.render('private/profile', { user: req.session.currentUser });
@@ -12,22 +11,16 @@ router.get('/rooms/add', (req, res) => {
 	res.render('rooms/new-room');
 });
 
-router.post('/rooms/add', fileUploader.single('imageUrl'), (req, res) => {
+router.post('/rooms/add', (req, res) => {
 	//Get the user id from the session
 	const userId = req.session.currentUser._id;
 
 	//Get the form data from the body
 	const { name, description } = req.body;
 
-	//Get the image url from uploading
-	const imageUrl = req.file.path
-
-	console.log(name, description, imageUrl);
-
 	Room.create({
 		name,
 		description,
-		imageUrl,
 		owner: userId
 	})
 		.then((createdRoom) => {
